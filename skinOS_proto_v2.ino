6fb52693b8678945b52de258b37351b32c686621
Matthew Epler <matthewepler@gmail.com>
2016-03-17 11:25:33 -0400

    lights and vibe motor working

--- skinOS_proto_v2.ino
+++ skinOS_proto_v2.ino
@@ -3,19 +3,19 @@
 
 #define PANELPIN 6
 Adafruit_NeoPixel panelStrip = Adafruit_NeoPixel(32, PANELPIN, NEO_GRB + NEO_KHZ800);
-
-CapacitiveSensor   cs_1 = CapacitiveSensor(5,12); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
-CapacitiveSensor   cs_2 = CapacitiveSensor(9,13); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
-
-
 int panelLeft[]   = {0, 1, 8, 9, 16, 17, 24, 25};
 int panelCenter[] = {3, 4, 11, 12, 19, 20, 27, 28};
 int panelRight[]  = {6, 7, 14, 15, 22, 23, 30, 31};
 
+CapacitiveSensor   cs_1 = CapacitiveSensor(5, 12); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
+CapacitiveSensor   cs_2 = CapacitiveSensor(9, 13); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
+boolean touch;
+int offCounter;
 
 void setup() {
   Serial.begin(9600);
-    
+  pinMode(2, OUTPUT);
+
   panelStrip.setBrightness(2);
   panelStrip.begin();
   panelStrip.show(); // Initialize all pixels to 'off'
@@ -24,30 +24,54 @@ void setup() {
 }
 
 void loop() {
-  long cap1 =  cs_1.capacitiveSensor(30);
-  long cap2 =  cs_2.capacitiveSensor(30);
-  Serial.print(cap1);
-  Serial.print(" : ");
-  Serial.println(cap2);
-  delay(10);
+  checkCapSensors(); // has 10ms delay
+
+  if (touch) {
+    analogWrite(10, 122);
+  } else {
+    analogWrite(10, 0);
+  }
+
+  Serial.println(touch);
 }
 
 void launchPanels() {
-  panelOn(panelLeft);
-  panelOn(panelCenter);
-  panelOn(panelRight);
+  panelSet(panelLeft);
+  panelSet(panelCenter);
+  panelSet(panelRight);
 }
 
-void panelOn(int *panel) { 
+void panelSet(int *panel) {
   int colorCounter = 0;
   while (colorCounter <= 127) {
-    for(int i = 0; i < 8; i++) {
-        panelStrip.setPixelColor(panel[i], panelStrip.Color(colorCounter,colorCounter,colorCounter));
-        panelStrip.show();
-        colorCounter += 3;
-        delay(10);
+    for (int i = 0; i < 8; i++) {
+      panelStrip.setPixelColor(panel[i], panelStrip.Color(colorCounter, colorCounter, colorCounter));
+      panelStrip.show();
+      colorCounter += 3;
+      delay(10);
     }
-  } 
+  }
+}
+
+void checkCapSensors() {
+  long cap1 =  cs_1.capacitiveSensor(30);
+  long cap2 =  cs_2.capacitiveSensor(30);
+  if ( cap1 > 200 && cap2 > 200) {
+    if (!touch) {
+      launchPanels();
+      touch = true;
+    }
+  } else {
+    if(touch) {
+      if (offCounter > 100) {
+        offCounter = 0;
+        touch = false;
+      } else {
+        offCounter++;
+      }
+    }
+  }
+  delay(10);
 }
 
 
