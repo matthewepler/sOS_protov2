e4955a6bf0af0887cde849e3a8a9deee7adbf1cb
Matthew Epler <matthewepler@gmail.com>
2016-03-17 10:20:33 -0400

    coming back to this - cap sensing working

--- skinOS_proto_v2.ino
+++ skinOS_proto_v2.ino
@@ -1,18 +1,12 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h>
-#endif
+#include <CapacitiveSensor.h>
 
-#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
-// Required for Serial on Zero based boards
-#define Serial SERIAL_PORT_USBVIRTUAL
-#endif
+#define PANELPIN 6
+Adafruit_NeoPixel panelStrip = Adafruit_NeoPixel(32, PANELPIN, NEO_GRB + NEO_KHZ800);
 
-const int coilPin = 5;
-int panelDelay = 500;
+CapacitiveSensor   cs_1 = CapacitiveSensor(5,12); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
+CapacitiveSensor   cs_2 = CapacitiveSensor(9,13); // 1M resistor between pins 5 & 12, pin 12 is sensor pin
 
-#define BOARDLEDS 6
-Adafruit_NeoPixel panelStrip = Adafruit_NeoPixel(32, BOARDLEDS, NEO_GRB + NEO_KHZ800);
 
 int panelLeft[]   = {0, 1, 8, 9, 16, 17, 24, 25};
 int panelCenter[] = {3, 4, 11, 12, 19, 20, 27, 28};
@@ -21,32 +15,36 @@ int panelRight[]  = {6, 7, 14, 15, 22, 23, 30, 31};
 
 void setup() {
   Serial.begin(9600);
-
-  panelStrip.setBrightness(50);
+    
+  panelStrip.setBrightness(2);
   panelStrip.begin();
   panelStrip.show(); // Initialize all pixels to 'off'
 
   //attachInterrupt(digitalPinToInterrupt(coilPin), panelOn, LOW);
-  
-  panelOn(panelLeft);
-  //delay(panelDelay);
-  panelOn(panelCenter);
-  //delay(panelDelay);
-  panelOn(panelRight);
 }
 
 void loop() {
-  //Serial.println(digitalRead(5));
-  
+  long cap1 =  cs_1.capacitiveSensor(30);
+  long cap2 =  cs_2.capacitiveSensor(30);
+  Serial.print(cap1);
+  Serial.print(" : ");
+  Serial.println(cap2);
+  delay(10);
+}
+
+void launchPanels() {
+  panelOn(panelLeft);
+  panelOn(panelCenter);
+  panelOn(panelRight);
 }
 
 void panelOn(int *panel) { 
   int colorCounter = 0;
-  while (colorCounter <= 255) {
+  while (colorCounter <= 127) {
     for(int i = 0; i < 8; i++) {
         panelStrip.setPixelColor(panel[i], panelStrip.Color(colorCounter,colorCounter,colorCounter));
         panelStrip.show();
-        colorCounter += 1;
+        colorCounter += 3;
         delay(10);
     }
   } 
